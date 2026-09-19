ddab48a29c8a47059ee04e6cf00f45df81a6d25a
juny030507 <juny030507@gmail.com>
2026-09-20 05:10:10 +0900

    feat: 재개 가능한 M2D 지도학습기와 순차 실행 구현

--- ml/m2d_supervised/m2d_supervised/__init__.py
+++ ml/m2d_supervised/m2d_supervised/__init__.py
@@ -1,3 +1,3 @@
-"""Reproducible M2D supervised-learning preparation for the Baby Care model."""
+"""Reproducible M2D supervised experiments for the Baby Care model."""
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
